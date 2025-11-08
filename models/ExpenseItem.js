10dbb489aecbe66a9fd252f707635a3f037f8d8e
Angel00705 <angeltar0070520@gmail.com>
2025-11-08 20:01:06 +0300

    Create ExpenseItem.js

---
+++ models/ExpenseItem.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const expenseItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('ExpenseItem', expenseItemSchema);
