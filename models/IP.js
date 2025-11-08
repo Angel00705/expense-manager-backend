d99e2001e2e6764f24dec87b890bc6cce1325941
Angel00705 <angeltar0070520@gmail.com>
2025-11-08 20:03:49 +0300

    Update IP.js

--- models/IP.js
+++ models/IP.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 
 const ipSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  region: { type: String, required: true },
-  notes: { type: String, default: '' }
-}, { timestamps: true });
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  region: {
+    type: String,
+    required: true
+  },
+  inn: {
+    type: String,
+    default: ''
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('IP', ipSchema);
